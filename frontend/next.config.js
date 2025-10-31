304afa3707a0a43826946d6d1ed499696b41412b
Shenulal HP <shenulal@hellopixels.com>
2025-10-31 14:53:35 +0400

    Simplify next.config.js - remove custom webpack config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,8 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    }
-    return config
   },
 }
 
